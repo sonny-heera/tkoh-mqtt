c496deaa8aa0c04b02659a821c2d71e8b1a741ef
Sonny Heera <81140924+sonny-heera@users.noreply.github.com>
2021-08-07 17:06:39 -0600

    Added frontend for broker info

---
+++ tkoh-mqtt/src/main/java/com/tkoh/iot/client/ui/HomeActivity.kt
@@ -0,0 +1,6 @@
+package com.tkoh.iot.client.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.tkoh.iot.client.R
+
+class HomeActivity : AppCompatActivity(R.layout.home_activity)
---
+++ tkoh-mqtt/src/main/java/com/tkoh/iot/client/ui/SelectedBrokerFragment.kt
@@ -0,0 +1,6 @@
+package com.tkoh.iot.client.ui
+
+import androidx.fragment.app.Fragment
+import com.tkoh.iot.client.R
+
+class SelectedBrokerFragment : Fragment(R.layout.selected_broker_fragment)
